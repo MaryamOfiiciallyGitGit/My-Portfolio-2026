b236bd903fe4788a88204e7dc11541168683a715
MaryamGit˚🍮🥄 ˚₊‧ <maryamofficially24@gmail.com>
2026-04-14 12:42:10 +0400

    uploud first

---
+++ spong-bob-first-project/Spongbob.java
@@ -0,0 +1,23 @@
+public class Spongbob {
+    public static void main(String[] args) {
+
+        String spongeBob = """
+               .--..--..--..--..--..--.
+             .' \\  (`._   (_)     _   \\
+           .'    |  '._)         (_)  |
+         \\ _.')\\      .----..---.   /
+         |(_.'  |     /    .-._.'    |
+         \\     0|    /    |   |      |
+          |  _  |  .-|    |   |      |
+          | (_) |   _|    |   |      |
+          |     |  |      |   |      |
+          |     |  |      |   |      |
+          |     |  |      |   |      |
+    
+           
+        """;
+
+        System.out.println(spongeBob);
+        System.out.println("spongebob squarepants");
+    }
+}
